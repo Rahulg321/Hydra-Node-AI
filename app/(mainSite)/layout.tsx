f655a084365a69e3b97a902fa235ca9deaf1f35b
Rahul Gupta <rg5353070@gmail.com>
2024-12-13 19:23:04 +0530

    fixed some hydration errors

--- app/(mainSite)/layout.tsx
+++ app/(mainSite)/layout.tsx
@@ -63,7 +63,7 @@ export default async function RootLayout({
 }>) {
   const session = await auth();
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Head>
         <link
           rel="icon"
